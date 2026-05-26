1d61fcb9a6fab7c9dbd7b52fd87736aeaa9f33ff
Brian-Entengshi <brianencina524@gmail.com>
2026-05-26 17:26:37 +0800

    quiz2

---
+++ Quiz2/eventproject/events/views.py
@@ -0,0 +1,20 @@
+
+# Create your views here.
+from django.shortcuts import render
+from .forms import EventRegistrationForm
+
+def register_event(request):
+    if request.method == 'POST':
+        form = EventRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return render(request, 'success.html')
+
+    else:
+        form = EventRegistrationForm()
+
+    return render(request, 'register.html', {
+        'form': form
+    })
